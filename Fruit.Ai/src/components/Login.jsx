2f598fd0b556cc6274a8c415bcc65ba36c518e72
anu-commits <anuragchauhan0612@gmail.com>
2024-09-16 11:47:12 +0530

    firstcommit

---
+++ Fruit.Ai/src/components/Login.jsx
@@ -0,0 +1,27 @@
+// Nirbhay singh
+// Author Nirbhay Singh
+import { useNavigate } from "react-router-dom";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        {navigate("/home")}
+        <UserButton>Show</UserButton>
+      </SignedIn>
+    </div>
+  );
+};
+
+export default Login;
